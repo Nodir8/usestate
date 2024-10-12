a0c23183e15daa3aa2d94de8b12dd04f951c0d55
Nodir8 <Nodirbek8464>
2024-10-13 00:22:11 +0900

    nodirrr

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
-function App() {
+const User = ({ firstName, lastName, Link }) => {
+  const [counter, setCount] = useState(0);
+  const [islogin, setIslogin] = useState(true);
+  const [age, setAge] = useState(21);
+
+  const onIncrement = () => {
+    setCount(counter + 1);
+  };
+  const onDecrement = () => {
+    setCount(counter - 1);
+  };
+  const Onrestart = () => {
+    setCount(0);
+  };
+  const onToggleLogin = () => {
+    setIslogin((prevState) => !prevState);
+  };
+
+  useEffect(() => {
+    console.log("effect");
+    document.title = `Counter: ${counter}`;
+    return () => console.log("deleted");
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-50 mx-auto">
+      <div className="border p-3 mt-5">
+        <h1>
+          mening ismim - {firstName}, sharifim -{lastName}
+        </h1>
+        <a href={Link}>Youtube Kanalim</a>
+        <p className="text-center">{counter}</p>
+
+        <div className="d-flex justify-content-center">
+          <button className="btn btn-success" onClick={onIncrement}>
+            +
+          </button>
+          <button className="btn btn-danger mx-2" onClick={onDecrement}>
+            -
+          </button>
+          <button className="btn btn-dark" onClick={Onrestart}>
+            0
+          </button>
+        </div>
+        {islogin ? <p className="text-center mt-3">Login</p> : null}
+        <div className="d-flex justify-content-center">
+          <button onClick={onToggleLogin} className="btn btn-outline-primary">
+            Toggle
+          </button>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
+const App = () => {
+  return (
+    <>
+      <User firstName="Samar" lastName="Badriddinov" Link="youtube.com" />
+      <User firstName="Omar" lastName="hikmatov" Link="youtube.com" />
+    </>
+  );
+};
 export default App;
